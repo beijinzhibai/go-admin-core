d27a018340a110f229e871de92d08f9d4932fa76
yangxh <yangxh0306@163.com>
2023-11-01 11:45:40 +0800

    第一次提交

---
+++ storage/cache/redis.go
@@ -0,0 +1,156 @@
+package cache
+
+import (
+	"context"
+	"github.com/go-redis/redis/v9"
+	"time"
+)
+
+// redis模式 集群模式
+func NewCusterRedis(client *redis.ClusterClient, addrs []string, options *redis.Options) (*ClusterRedis, error) {
+	if client == nil {
+		client = redis.NewClusterClient(&redis.ClusterOptions{
+			Addrs:    addrs,
+			Password: options.Password,
+		})
+	}
+	r := &ClusterRedis{
+		client: client,
+	}
+	err := r.connect()
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+
+}
+
+// NewRedis redis模式
+func NewRedis(client *redis.Client, options *redis.Options) (*Redis, error) {
+	if client == nil {
+		client = redis.NewClient(options)
+	}
+	r := &Redis{
+		client: client,
+	}
+	err := r.connect()
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+type ClusterRedis struct {
+	client *redis.ClusterClient
+}
+
+func (r *ClusterRedis) String() string {
+	return "redis"
+}
+
+func (r *ClusterRedis) Get(key string) (string, error) {
+	return r.client.Get(context.TODO(), key).Result()
+}
+
+func (r *ClusterRedis) Set(key string, val interface{}, expire int) error {
+	return r.client.Set(context.TODO(), key, val, time.Duration(expire)*time.Second).Err()
+}
+
+func (r *ClusterRedis) Del(key string) error {
+	return r.client.Del(context.TODO(), key).Err()
+}
+
+func (r *ClusterRedis) HashGet(hk, key string) (string, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *ClusterRedis) HashDel(hk, key string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *ClusterRedis) Increase(key string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *ClusterRedis) Decrease(key string) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *ClusterRedis) Expire(key string, dur time.Duration) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+type Redis struct {
+	client *redis.Client
+}
+
+func (*Redis) String() string {
+	return "redis"
+}
+
+// connect connect test
+func (r *Redis) connect() error {
+	var err error
+	_, err = r.client.Ping(context.TODO()).Result()
+	return err
+}
+
+func (r *ClusterRedis) connect() error {
+	var err error
+	_, err = r.client.Ping(context.TODO()).Result()
+	return err
+}
+
+// Get from key
+func (r *Redis) Get(key string) (string, error) {
+	return r.client.Get(context.TODO(), key).Result()
+}
+
+// Set value with key and expire time
+func (r *Redis) Set(key string, val interface{}, expire int) error {
+	return r.client.Set(context.TODO(), key, val, time.Duration(expire)*time.Second).Err()
+}
+
+// Del delete key in redis
+func (r *Redis) Del(key string) error {
+	return r.client.Del(context.TODO(), key).Err()
+}
+
+// HashGet from key
+func (r *Redis) HashGet(hk, key string) (string, error) {
+	return r.client.HGet(context.TODO(), hk, key).Result()
+}
+
+// HashDel delete key in specify redis's hashtable
+func (r *Redis) HashDel(hk, key string) error {
+	return r.client.HDel(context.TODO(), hk, key).Err()
+}
+
+// Increase
+func (r *Redis) Increase(key string) error {
+	return r.client.Incr(context.TODO(), key).Err()
+}
+
+func (r *Redis) Decrease(key string) error {
+	return r.client.Decr(context.TODO(), key).Err()
+}
+
+// Set ttl
+func (r *Redis) Expire(key string, dur time.Duration) error {
+	return r.client.Expire(context.TODO(), key, dur).Err()
+}
+
+// GetClient 暴露原生client
+func (r *Redis) GetClient() *redis.Client {
+	return r.client
+}
+
+// GetClient 暴露原生client
+func (r *ClusterRedis) GetClusterClient() *redis.ClusterClient {
+	return r.client
+}
